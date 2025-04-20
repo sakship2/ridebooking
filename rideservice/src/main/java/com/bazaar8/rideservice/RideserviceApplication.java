13ec82eb6775e6e421833c5f95dba92898c78703
Sakshi Patel <patelsakship22@gmail.com>
2025-04-20 20:43:37 +0530

    initial

---
+++ rideservice/src/main/java/com/bazaar8/rideservice/RideserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bazaar8.rideservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideserviceApplication.class, args);
+	}
+
+}
